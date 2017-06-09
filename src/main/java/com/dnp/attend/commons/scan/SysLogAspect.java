d2e620ff4a50598ce0e539c7f647a632752a4b86
huazai <379924269@qq.com>
2017-06-09 17:01:29 +0800

    1、用户controller写了一个基本的继承，继承了一些简单的方法
    2、记录系统日志的，加了一个，还可以（需要进一步去学习看看）
    
    3、swaggerUI3.0发送的请求sessionId老是变，（看看怎么处理，我是换成了2.几的）
    
    4、添加了本地日志文件

---
+++ src/main/java/com/dnp/attend/commons/scan/SysLogAspect.java
@@ -0,0 +1,76 @@
+package com.dnp.attend.commons.scan;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/** 
+ * Description: 这个记日志还不错
+ * 
+ * @author xhxing
+ * @since 2017年6月9日 下午12:12:23  
+ */ 
+@Aspect
+@Component
+@Order
+public class SysLogAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SysLogAspect.class);
+
+//    @Autowired
+//    private ISysLogService sysLogService;
+
+    @Pointcut("execution(* com.dnp.attend.controller.*Controller.*(..))")
+    public void cutController() {}
+
+	@SuppressWarnings("unchecked")
+	@Around("cutController()")
+    public Object recordSysLog(ProceedingJoinPoint point) throws Throwable {
+        String strMethodName = point.getSignature().getName();
+        String strClassName = point.getTarget().getClass().getName();
+        Object[] params = point.getArgs();
+        StringBuffer bfParams = new StringBuffer();
+        Enumeration<String> paraNames = null;
+        HttpServletRequest request = null;
+        if (params != null && params.length > 0) {
+            request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            paraNames = request.getParameterNames();
+            String key;
+            String value;
+            while (paraNames.hasMoreElements()) {
+                key = paraNames.nextElement();
+                value = request.getParameter(key);
+                bfParams.append(key).append("=").append(value).append("&");
+            }
+            if (StringUtils.isBlank(bfParams)) {
+                bfParams.append(request.getQueryString());
+            }
+        }
+
+        String strMessage = String
+                .format("[类名]:%s,[方法]:%s,[参数]:%s", strClassName, strMethodName, bfParams.toString());
+        LOGGER.info(strMessage);
+        return point.proceed();
+    }
+
+//    private boolean isWriteLog(String method) {
+//        String[] pattern = {"login", "logout", "add", "edit", "delete", "grant"};
+//        for (String s : pattern) {
+//            if (method.indexOf(s) > -1) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+}
--- src/main/java/com/dnp/attend/controller/UserController.java
+++ src/main/java/com/dnp/attend/controller/UserController.java
@@ -1,23 +1,24 @@
 package com.dnp.attend.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.ServletRequestDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.dnp.attend.commons.base.BaseController;
 import com.dnp.attend.model.User;
 import com.dnp.attend.service.UserService;
 import com.dnp.attend.vo.PageVo;
@@ -32,7 +33,7 @@ import com.dnp.attend.vo.PageVo;
 @Api(value = "UserController", description = "用户信息")
 @RestController
 @RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-public class UserController {
+public class UserController extends BaseController {
 
 	@Autowired
 	private UserService userService;
@@ -41,6 +42,9 @@ public class UserController {
 	@ApiOperation(value = "查询所有用户信息", notes = "查询所有用户信息")
 	public Object findAll(PageVo pageVo,
 			@ApiParam(name = "search", value = "模糊查询字段", required = false) @RequestParam(required = false, defaultValue = "") String search) {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+				.getRequestAttributes()).getRequest();
+		System.out.println(request.getSession().getId());
 		User user = new User();
 		return userService.selectByExample(pageVo, search, "", user);
 	}
@@ -53,36 +57,28 @@ public class UserController {
 
 	@RequestMapping(value = "", method = RequestMethod.PUT)
 	@ApiOperation(value = "修改用户信息", notes = "修改用户信息")
-	public void update(User user) {
-		userService.updateAll(user);
+	public Object update(User user) {
+		userService.updateNotNull(user);
+		return renderSuccess();
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	@ApiOperation(value = "添加用户信息", notes = "添加用户信息")
-	public void save(@ApiParam(name = "name", value = "用户名称", required = false) @RequestParam(required = false) String name,
+	public Object save(@ApiParam(name = "name", value = "用户名称", required = false) @RequestParam(required = false) String name,
 			@ApiParam(name = "email", value = "用户email", required = false) @RequestParam(required = false) String email,
 			@ApiParam(name = "password", value = "用户密码", required = false) @RequestParam(required = false) String password,
 			@ApiParam(name = "createdDate", value = "用户创建日期", required = false) @RequestParam(required = false) Date createdDate,
 			@ApiParam(name = "roleId", value = "角色id", required = false) @RequestParam(required = false) Integer roleId) {
 		User user = new User(name, email, password, createdDate, roleId);
 		userService.save(user);
+		return renderSuccess();
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.DELETE)
 	@ApiOperation(value = "删除用户信息", notes = "修改用户信息")
-	public void delete(@ApiParam(name = "id", value = "用户信息id", required = true) @PathVariable("id") Integer id) {
+	public Object delete(@ApiParam(name = "id", value = "用户信息id", required = true) @PathVariable("id") Integer id) {
 		userService.delete(id);
+		renderSuccess();
+		return renderSuccess();
 	}
-
-	@InitBinder
-	public void initBinder(ServletRequestDataBinder binder) {
-		/**
-		 * 自动转换日期类型的字段格式
-		 */
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), true));
-		/**
-		 * 防止XSS攻击
-		 */
-	}
-
 }
